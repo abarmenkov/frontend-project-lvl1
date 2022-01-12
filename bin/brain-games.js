cd1241547704b88c644363353000b67a5ebc40e5
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-01-12 21:14:13 +0300

    test2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import { greetGamer } from '../src/cli.js';
+/* eslint-disable import/extensions */
+
+import greetGamer from '../src/cli.js';
 
 console.log("Welcome to the Brain Games!");
 greetGamer();
