51a5857dd753bb286479bbdcb488f36e7299a83e
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-01-12 19:41:41 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export function greetGamer() {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + name + '!');
 }
