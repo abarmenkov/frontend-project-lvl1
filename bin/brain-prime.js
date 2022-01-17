428e572ae3c59269a285cd3934941d7092579c35
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-01-17 22:01:27 +0300

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line import/extensions
+import init from '../src/games/brain-prime/brainPrime.js';
+
+init();
---
+++ src/games/brain-prime/brainPrime.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable sort-imports */
+/* eslint-disable import/extensions */
+/* eslint-disable no-ternary */
+/* eslint-disable no-magic-numbers */
+/* eslint-disable func-names */
+/* eslint-disable class-methods-use-this */
+// eslint-disable-next-line import/extensions
+import readlineSync from 'readline-sync';
+import GamesSettings from "../gamesSettings.js";
+import startGame from "../index.js";
+
+class BrainPrime extends GamesSettings {
+  positiveAnswer = 'yes';
+
+  negativeAnswer = 'no';
+
+  gameRules = function() {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  };
+
+  isPrime = function(num) {
+    // eslint-disable-next-line id-length
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return num > 1;
+  };
+
+  guessCheck = function (num) {
+    return this.isPrime(num) ? this.positiveAnswer : this.negativeAnswer;
+  };
+
+  gamerGuess = function () {
+    this.guess = readlineSync.question(`Question:  ${this.generatedNumber} `);
+  };
+
+  getGuessResult = function (number, guess) {
+    // eslint-disable-next-line max-len
+    return (this.isPrime(number) && guess === this.positiveAnswer) || (!this.isPrime(number) && guess === this.negativeAnswer);
+  };
+
+  playGame = function () {
+    while (this.roundsNumber > 0) {
+      this.generatedNumber = this.generateNumber(this.maxNumber);
+      this.gamerGuess();
+      this.printAnswer();
+      this.result = this.getGuessResult(this.generatedNumber, this.guess);
+      this.correctAnswer = this.guessCheck(this.generatedNumber);
+      if (!this.checkResult(this.result)) {
+        return false;
+      }
+    }
+    return true;
+  };
+}
+
+export default function init(roundsNumber, maxNumber) {
+  const game = new BrainPrime(roundsNumber, maxNumber);
+  startGame(game);
+}
