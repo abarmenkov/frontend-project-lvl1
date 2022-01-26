67f672fd00cd772db1250254342e1719f0ceaf39
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-01-26 12:14:49 +0300

    refactor

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -3,20 +3,20 @@ import startGame from '../index.js';
 
 const rule = 'What number is missing in the progression?';
 
-const generateProgression = (firstNumber, progressionStep, arrLength) => {
+const generateProgression = (firstNumber, progressionStep, progressionLength) => {
   const arr = [firstNumber];
-  for (let i = 0; i < arrLength; i += 1) {
+  for (let i = 0; i < progressionLength; i += 1) {
     arr.push(arr[i] + progressionStep);
   }
   return arr;
 };
 
 const progressionGame = () => {
-  const arrLength = generateNumber(6, 10);
+  const progressionLength = generateNumber(6, 10);
   const progressionStep = generateNumber(1, 10);
   const firstNumber = generateNumber(1, 10);
-  const hideIndex = generateNumber(0, arrLength - 1);
-  const progression = generateProgression(firstNumber, progressionStep, arrLength);
+  const hideIndex = generateNumber(0, progressionLength - 1);
+  const progression = generateProgression(firstNumber, progressionStep, progressionLength);
   const correctAnswer = String(progression[hideIndex]);
   progression[hideIndex] = '..';
   return [progression.join(' '), correctAnswer];
